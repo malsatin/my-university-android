22c1cd9c1cbc53d841769754e58e22a826f3ed95
Valeria <v.ahmetzhanova@gmail.com>
2020-05-12 22:34:59 +0300

    added protocol prefixes

--- app/src/main/java/com/example/myuniversityclient/data/repository/main/ITLinksServiceMock.kt
+++ app/src/main/java/com/example/myuniversityclient/data/repository/main/ITLinksServiceMock.kt
@@ -11,10 +11,10 @@ class ITLinksServiceMock: ITLinksService {
     override fun getITServices(onResult: (Result<ITServicesList?>) -> Unit) {
         val mockList = ITServicesList(
             listOf(
-                ITService ("eDisk", "Shared file storage (SMB, HTTPS, SFTP)", "edisk.university.innopolis.ru"),
+                ITService ("eDisk", "Shared file storage (SMB, HTTPS, SFTP)", "https://edisk.university.innopolis.ru"),
                 ITService ("IT Support", "Send request to Innopolis University IT Department", "mailto:it@innopolis.ru"),
-                ITService ("Mail", "Web access to email", "mail.innopolis.ru"),
-                ITService ("Moodle", "Curriculum selection portal", "moodle.university.innopolis.ru")
+                ITService ("Mail", "Web access to email", "https://mail.innopolis.ru"),
+                ITService ("Moodle", "Curriculum selection portal", "https://moodle.university.innopolis.ru")
                 )
         );
 
