c4276ab4f1f449941ed266418f538fd7256671e9
Valeria <v.ahmetzhanova@gmail.com>
2020-05-12 22:20:46 +0300

    added real services to the mock data

--- app/src/main/java/com/example/myuniversityclient/data/repository/main/ITLinksServiceMock.kt
+++ app/src/main/java/com/example/myuniversityclient/data/repository/main/ITLinksServiceMock.kt
@@ -10,7 +10,12 @@ import com.example.myuniversityclient.data.models.ITServicesList
 class ITLinksServiceMock: ITLinksService {
     override fun getITServices(onResult: (Result<ITServicesList?>) -> Unit) {
         val mockList = ITServicesList(
-            listOf(ITService ("test", "test", "test"))
+            listOf(
+                ITService ("eDisk", "Shared file storage (SMB, HTTPS, SFTP)", "edisk.university.innopolis.ru"),
+                ITService ("IT Support", "Send request to Innopolis University IT Department", "mailto:it@innopolis.ru"),
+                ITService ("Mail", "Web access to email", "mail.innopolis.ru"),
+                ITService ("Moodle", "Curriculum selection portal", "moodle.university.innopolis.ru")
+                )
         );
 
         onResult(Result.success(mockList))
